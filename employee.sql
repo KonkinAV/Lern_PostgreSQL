f07d0fd2d45511080b915ef61c7fe2f1f05154c6
Alexandr <58111714+KonkinAV@users.noreply.github.com>
2025-11-03 19:11:33 +0300

    Delete

--- employee.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE employees (
-    id INT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    department VARCHAR(100),
-    manager_id INT,
-    FOREIGN KEY (manager_id) REFERENCES employees(id)
-);
